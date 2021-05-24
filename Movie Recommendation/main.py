3bdfc1e1e7d618e01a5aa4048cfed62d7ff6d537
Rushikesh Patade <72869428+Rushour0@users.noreply.github.com>
2021-05-24 16:05:58 +0530

    Update main.py

--- Movie Recommendation/main.py
+++ Movie Recommendation/main.py
@@ -12,21 +12,26 @@ async def on_ready():  # When the bot is ready
     print("I'm in")
     print(bot.user)  # Prints the bot's username and identifier
 
-
+# Command to find recommended movie based on overview of the movies
 @bot.command()
 async def recommend_title(ctx, *args):
 	movie_name = ' '.join(args)
 	print(movie_name)
 	print(f"User: {ctx.author.name}\nGuild: {ctx.guild}\nMovie: {movie_name}\nType: Title")
+	
+	# Writing logs of the tasks performed
 	with open("logs.txt","a") as logs:
 		logs.write(f"User: {ctx.author.name}\nGuild: {ctx.guild}\nMovie: {movie_name}\nType: Title")
+	
 	try:
 		recommended_movies = overview_recommend.give_only_title(' '.join([i.capitalize() for i in args]))
 	except:
 		recommended_movies = overview_recommend.give_only_title(movie_name)
+	
 	print('\n'.join(recommended_movies))
 	await ctx.send('\n'.join(recommended_movies))
 
+# Command to find recommended movie based on overview of the movies as well as score
 @bot.command()
 async def recommend_title_score(ctx, *args):
 	movie_name = ' '.join(args)
@@ -41,5 +46,6 @@ async def recommend_title_score(ctx, *args):
 	print('\n'.join(recommended_movies))
 	await ctx.send('\n'.join(recommended_movies))
 
+# Running the discord bot
 token = os.environ["DISCORD_BOT_SECRET"]
 bot.run(token)
