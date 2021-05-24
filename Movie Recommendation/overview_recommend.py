9acc4a72e9ee7bc83cd271996064accb81a48fd1
Rushikesh Patade <72869428+Rushour0@users.noreply.github.com>
2021-05-24 16:00:43 +0530

    Update overview_recommend.py

--- Movie Recommendation/overview_recommend.py
+++ Movie Recommendation/overview_recommend.py
@@ -4,6 +4,7 @@ from sklearn.metrics.pairwise import sigmoid_kernel
 
 movies_preprocessed = pd.read_csv("preprocessed.csv")
 
+# the Text-Frequency Inverse Document Frequency Vectorizer's parameters are set here.
 tfv = TfidfVectorizer(min_df=3,
                       max_features=None,
                       strip_accents='unicode',
@@ -11,25 +12,27 @@ tfv = TfidfVectorizer(min_df=3,
                       token_pattern=r'\w{1,}',
                       ngram_range=(1, 3),
                       stop_words='english')
-
+# Filling in empty spaces
 movies_preprocessed['overview'] = movies_preprocessed['overview'].fillna('')
 
+# Making use of the TFIDF on the preprocessed document
 tfv_matrix = tfv.fit_transform(movies_preprocessed['overview'])
 
+# Sigmoid_kernel function to essentially match the words from the created matrix to other words in the same matrix itself
 sig = sigmoid_kernel(tfv_matrix, tfv_matrix)
 
 # Reverse mapping of indices and movie titles
-indices = pd.Series(
-    movies_preprocessed.index,
-    index=movies_preprocessed['original_title']).drop_duplicates()
+indices = pd.Series(movies_preprocessed.index,index=movies_preprocessed['original_title']).drop_duplicates()
 
-
+# Movie recommendation based on title as well as score
 def give_title_score(title, sig=sig):
+	
     # Get the index corresponding to original_title
     idx = indices[title]
 
     # Get the pairwsie similarity scores
     sig_scores = list(enumerate(sig[idx]))
+	
 
     # Sort the movies
     sig_scores = sorted(sig_scores, key=lambda x: x[1], reverse=True)
@@ -48,6 +51,7 @@ def give_title_score(title, sig=sig):
     return_movies = [list(row)[0] for row in return_df[:10].values]
     return return_movies
 
+# Movie recommendation based on title only
 def give_only_title(title, sig=sig):
     # Get the index corresponding to original_title
     idx = indices[title]
